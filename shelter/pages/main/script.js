9981e192138c800a7f039cfde78bbefdb542880d
onc157 <onc157@yandex.ru>
2020-10-19 00:23:35 +0300

    fix:main: implement closed burger when we click on first item in burger menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -2,6 +2,7 @@ let burgerMenu = document.querySelector('.header-burger-menu');
 let burgerLogo = document.querySelector('.header-burger');
 let headerLogo = document.querySelector('.header-logo');
 let burgerWrapper = document.querySelector('.burger-wrapper');
+let firstItemInBurgerList = document.querySelector('.burger-list').children[0];
 
 const closeBurger = () => {
     burgerLogo.classList.toggle('header-burger--active');
@@ -12,6 +13,7 @@ const closeBurger = () => {
 
 burgerLogo.addEventListener('click', () => closeBurger());
 burgerWrapper.addEventListener('click', () => closeBurger());
+firstItemInBurgerList.addEventListener('click', () => closeBurger());
 burgerMenu.addEventListener('click', (event) => event.stopPropagation())
 
 
