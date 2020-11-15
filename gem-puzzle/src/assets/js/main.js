8138e25c75217d011b646e75c182be4ce2b53319
onc157 <onc157@yandex.ru>
2020-11-16 02:18:48 +0300

    rafactor: change design: title/buttons/ feat: add interactive 2 level menu feat: implement field size selection in the menu feat: implement button for show numbers on cells feat: implement messages when press on button in menu refactor: add all create DOM items in constructor fix: button sounds fix: preHandler

--- gem-puzzle/src/assets/js/main.js
+++ gem-puzzle/src/assets/js/main.js
@@ -7,30 +7,22 @@ import audioClick from '../audio/click-cell.wav';
 import audioButton from '../audio/click-button.wav';
 import bgGenerate from './utils/bgGenerate';
 
-// Init DOM element
-const main = create('div', 'game-wrapper',
-  [create('div', 'title', 'Gem Puzzles!')]);
-const counts = create('div', 'counts-wrapper', [
-  create('div', 'count count-time', 'Moves: 0'),
-  create('div', 'count count-move', 'Count time!'),
-]);
-const buttons = create('div', 'buttons-wrapper', [
-  create('button', 'button new-game', 'New Game'),
-  create('button', 'button save-game', 'Save Game'),
-  create('button', 'button load-game', 'Load Game'),
-  create('button', 'button sound', '<span class="material-icons">volume_up</span>'),
-  create('button', 'button score', 'Score'),
-  create('button', 'button field-size', 'Field Size'),
-]);
-const soundWrapper = create('div', 'sound-wrapper', [
-  create('audio', null, null, null, ['src', audioClick]),
-  create('audio', null, null, null, ['src', audioButton]),
-]);
-
 export default class Game {
   constructor(size) {
     this.size = size;
-    this.field = create('div', 'field', null, main);
+    // Init DOM element
+    this.main = create('div', 'game-wrapper',
+      [create('div', 'title', 'Gem Puzzles by onc157')]);
+    this.fieldWrapper = create('div', 'field-wrapper', null, this.main);
+    this.field = create('div', 'field', null, this.fieldWrapper);
+    this.counts = create('div', 'counts-wrapper', [
+      create('div', 'count count-time', 'Moves: 0'),
+      create('div', 'count count-move', 'Count time!'),
+    ]);
+    this.soundWrapper = create('div', 'sound-wrapper', [
+      create('audio', null, null, null, ['src', audioClick]),
+      create('audio', null, null, null, ['src', audioButton]),
+    ]);
     this.menu = create('div', 'menu', [
       create('button', 'button new-game', 'New Game'),
       create('button', 'button save-game', 'Save Game'),
@@ -38,12 +30,23 @@ export default class Game {
       create('button', 'button sound', '<span class="material-icons">volume_up</span>'),
       create('button', 'button score', 'Score'),
       create('button', 'button field-size', 'Field Size'),
-    ], this.field);
+      create('button', 'button show-numbers', 'Show Numbers'),
+    ], this.fieldWrapper);
+    this.menuFieldSize = create('div', 'menu-field-size', [
+      create('button', 'button button-size', '3 x 3', null, ['id', '3']),
+      create('button', 'button button-size', '4 x 4', null, ['id', '4']),
+      create('button', 'button button-size', '5 x 5', null, ['id', '5']),
+      create('button', 'button button-size', '6 x 6', null, ['id', '6']),
+      create('button', 'button button-size', '7 x 7', null, ['id', '7']),
+      create('button', 'button button-size', '8 x 8', null, ['id', '8']),
+      create('button', 'button back', 'Back'),
+    ], this.fieldWrapper);
+    this.buttonPress = create('div', 'button-press', null, this.fieldWrapper);
     this.score = create('div', 'score-wrapper', 'score');
-    this.buttons = buttons;
     this.sizeCell = 100 / this.size;
-    this.menuButton = create('div', 'count button-menu', '<span class="material-icons">build</span>', counts);
+    this.menuButton = create('div', 'count button-menu', '<span class="material-icons">build</span>', this.counts);
     this.soundOn = true;
+    this.numbersOn = false;
     this.countMoves = 0;
     this.countTime = 0;
     this.backgroundImage = bgGenerate(1, 20);
@@ -51,50 +54,48 @@ export default class Game {
 
   // Add element on HTML markup
   init() {
-    document.body.prepend(main, counts, buttons, soundWrapper, this.score);
-    this.buttons.addEventListener('click', this.handlerEvent);
+    document.body.prepend(this.main, this.counts, this.soundWrapper, this.score);
     this.menuButton.addEventListener('click', this.handlerEvent);
-    this.menu.addEventListener('click', this.handlerEvent);
+    this.menu.addEventListener('mousedown', this.handlerEvent);
+    this.menuFieldSize.addEventListener('mousedown', this.handlerEvent);
     return this;
+  }
+
+  // Generate random solvable array
+  generateRandomArray() {
+    const randomNumbers = [...Array(this.size ** 2 - 1).keys()].map((elem) => elem + 1)
+      .sort(() => Math.random() - 0.5);
+    randomNumbers.push(0);
+
+    let count = 0;
+    for (let i = 0; i < this.size ** 2; i += 1) {
+      if (randomNumbers[i]) {
+        for (let j = 0; j < i; j += 1) {
+          if (randomNumbers[j] > randomNumbers[i]) {
+            count += 1;
+          }
+        }
+      }
+    }
+
+    for (let i = 0; i < this.size ** 2; i += 1) {
+      if (randomNumbers[i] === 0) {
+        count += Math.floor(1 + (i / Math.sqrt(this.size ** 2)));
+      }
+    }
+
+    if (((this.size ** 2) % 2 === 0) && (count % 2 === 0)) {
+      return randomNumbers;
+    }
+    if (((this.size ** 2) % 2 !== 0) && (count % 2 !== 0)) {
+      return randomNumbers;
+    }
+    return this.generateRandomArray();
   }
 
   generateField() {
     this.cells = [];
-    this.backgroundImage = bgGenerate(1, 20);
-
-    // Generate random solvable array
-    const generateRandomArray = () => {
-      const randomNumbers = [...Array(this.size ** 2 - 1).keys()].map((elem) => elem + 1)
-        .sort(() => Math.random() - 0.5);
-      randomNumbers.push(0);
-
-      let count = 0;
-      for (let i = 0; i < this.size ** 2; i += 1) {
-        if (randomNumbers[i]) {
-          for (let j = 0; j < i; j += 1) {
-            if (randomNumbers[j] > randomNumbers[i]) {
-              count += 1;
-            }
-          }
-        }
-      }
-
-      for (let i = 0; i < this.size ** 2; i += 1) {
-        if (randomNumbers[i] === 0) {
-          count += Math.floor(1 + (i / Math.sqrt(this.size ** 2)));
-        }
-      }
-
-      if (((this.size ** 2) % 2 === 0) && (count % 2 === 0)) {
-        return randomNumbers;
-      }
-      if (((this.size ** 2) % 2 !== 0) && (count % 2 !== 0)) {
-        return randomNumbers;
-      }
-      return generateRandomArray();
-    };
-
-    const randomNumbers = generateRandomArray();
+    this.randomNumbers = this.generateRandomArray();
 
     // Generate puzzle cells
     for (let i = 0; i < this.size ** 2 - 1; i += 1) {
@@ -102,22 +103,24 @@ export default class Game {
       const top = (i - left) / this.size;
 
       this.cell = create('div', 'cell-outer',
-        create('div', 'cell-inner', `${randomNumbers[i]}`, null), this.field,
-        ['id', `${randomNumbers[i]}`],
+        create('div', 'cell-inner', `${this.randomNumbers[i]}`, null), this.field,
+        ['id', `${this.randomNumbers[i]}`],
         ['style', `top: ${top * this.sizeCell}%; left: ${left * this.sizeCell}%;`]);
 
       // Set cells cizes
       this.cell.style.width = `${this.sizeCell}%`;
       this.cell.style.height = `${this.sizeCell}%`;
 
-      // Push empty cell
       this.cells.push({
-        number: randomNumbers[i],
+        number: this.randomNumbers[i],
         left: left,
         top: top,
         item: this.cell,
         active: false,
       });
+
+      // Set font size cells depend with cell size
+      this.cell.firstChild.style.fontSize = `${8 / this.size}rem`;
 
       this.cell.firstChild.addEventListener('click', () => this.cellMove(i));
 
@@ -130,29 +133,30 @@ export default class Game {
       this.cell.firstChild.style.backgroundPosition = `${(numberCell % this.size) * multiplier}% ${Math.floor(numberCell / this.size) * multiplier}%`;
     }
 
+    // Push empty cell
     this.emptyCell = {
       number: this.cells.length + 1,
       left: this.cells.length % this.size,
       top: (this.cells.length - (this.cells.length % this.size)) / this.size,
     };
-
     this.cells.push(this.emptyCell);
-    console.log(this.cells);
+
     this.updateCountTimes();
   }
 
   handlerEvent = (e) => {
-    console.log('e', e);
-    console.log('e.target.className', e.target.className);
     // BUTTONS HANDLERS
+    const playSound = () => {
+      if (this.soundOn) {
+        this.soundWrapper.childNodes[1].currentTime = 0;
+        this.soundWrapper.childNodes[1].play();
+      }
+    };
 
-    if (this.soundOn) {
-      soundWrapper.childNodes[1].currentTime = 0;
-      soundWrapper.childNodes[1].play();
-    }
+    if (e.target.className.match(/button|material/)) playSound();
 
     if (e.target.className.match(/sound/)
-        || e.target.innerText.match(/volume_up|volume_off/)) {
+        || e.target.innerText.match(/^volume_up$|^volume_off$/)) {
       this.switchSound(e);
     }
     if (e.target.className.match(/new-game/)) {
@@ -168,11 +172,23 @@ export default class Game {
     if (e.target.className.match(/load-game/)) {
       this.loadGame();
     }
-    if (e.target.className.match(/test-move/)) {
-      this.testMove();
-    }
     if (e.target.className.match(/score/)) {
       this.getScore();
+    }
+    if (e.target.className.match(/field-size/)) {
+      this.changeFieldSize();
+    }
+    if (e.target.className.match(/back/)) {
+      this.menuFieldSize.classList.toggle('menu-field-size--active');
+    }
+    if (e.target.className.match(/button-size/)) {
+      this.size = e.target.id;
+      this.sizeCell = 100 / this.size;
+      this.newGame();
+      this.menuFieldSize.classList.toggle('menu-field-size--active');
+    }
+    if (e.target.className.match(/show-numbers/)) {
+      this.showNumbers(e);
     }
   }
 
@@ -182,8 +198,8 @@ export default class Game {
     const topDiff = Math.abs(this.emptyCell.top - cell.top);
 
     if (this.soundOn) {
-      soundWrapper.childNodes[0].currentTime = 0;
-      soundWrapper.childNodes[0].play();
+      this.soundWrapper.childNodes[0].currentTime = 0;
+      this.soundWrapper.childNodes[0].play();
     }
 
     // We process click of only adjacent cells horizontall or verticall
@@ -239,82 +255,109 @@ export default class Game {
 
   // Start New Game without reload page
   newGame() {
-    console.log('new game!');
+    this.menu.classList.toggle('menu--active');
 
-    setTimeout(this.menu.classList.toggle('menu--active'), 100);
-    this.field.remove();
-    this.field = create('div', 'field', null, main);
-    this.menu = create('div', 'menu', [
-      create('button', 'button new-game', 'New Game'),
-      create('button', 'button save-game', 'Save Game'),
-      create('button', 'button load-game', 'Load Game'),
-      create('button', 'button sound', '<span class="material-icons">volume_up</span>'),
-      create('button', 'button score', 'Score'),
-      create('button', 'button field-size', 'Field Size'),
-    ], this.field);
-    this.menu.addEventListener('click', this.handlerEvent);
-    this.countTime = 0;
-    this.countMoves = 0;
-    counts.firstChild.innerHTML = `Moves: ${this.countMoves}`;
-    clearTimeout(this.timeOut);
-    this.generateField();
+    const newField = () => {
+      this.field.remove();
+      this.field = create('div', 'field', null, this.fieldWrapper);
+      this.countTime = 0;
+      this.countMoves = 0;
+      this.counts.firstChild.innerHTML = `Moves: ${this.countMoves}`;
+      clearTimeout(this.timeOut);
+      this.backgroundImage = bgGenerate(1, 20);
+      this.generateField();
+      this.btnPress('NEW GAME');
+
+      if (this.numbersOn === true) {
+        this.menu.lastChild.innerText = 'Hide Numbers';
+        this.cells.forEach((elem) => {
+          if (!elem.item) return;
+          elem.item.firstChild.classList.add('cell-inner--show-numbers');
+        });
+      }
+    };
+
+    setTimeout(newField, 500);
   }
 
   saveGame() {
-    // this.storageCell = this.cells;
     storage.set('Save Game', this.cells);
     storage.set('Field Size', this.size);
-    console.log('this.cells', this.cells);
-    console.log('saved games');
+    storage.set('Background Image', this.backgroundImage);
+    storage.set('numbersOn', this.numbersOn);
+    this.btnPress('GAME SAVED');
   }
 
   loadGame() {
+    if (!storage.get('Save Game')) {
+      this.btnPress('NO SAVED GAME');
+      return;
+    }
     this.cells = [];
-    this.storageCell = [];
-    this.storageCell = storage.get('Save Game');
-    console.log(this.storageCell);
-    // const storageNewArr = this.storageCell.map((elem) => elem.number);
-    // console.log(storageNewArr);
+    this.storageCells = [];
+    this.storageCells = storage.get('Save Game');
     this.size = storage.get('Field Size');
     this.sizeCell = 100 / this.size;
-    console.log(this.size);
+    this.backgroundImage = storage.get('Background Image');
+    this.numbersOn = storage.get('numbersOn');
 
-    this.field.remove();
-    this.field = create('div', 'field', null, main);
-    this.storageCell.forEach((elem, i) => {
-      if (i === this.storageCell.length - 1) {
-        this.emptyCell = {
+    this.menu.classList.toggle('menu--active');
+
+    setTimeout(() => {
+      this.field.remove();
+      this.field = create('div', 'field', null, this.fieldWrapper);
+      this.storageCells.forEach((elem, i) => {
+        if (i === this.storageCells.length - 1) {
+          this.emptyCell = {
+            number: elem.number,
+            left: elem.left,
+            top: elem.top,
+          };
+
+          this.cells.push(this.emptyCell);
+
+          return;
+        }
+        this.cell = create('div', 'cell-outer',
+          create('div', 'cell-inner', `${elem.number}`, null), this.field,
+          ['id', `${elem.number}`],
+          ['style', `top: ${elem.top * this.sizeCell}%; left: ${elem.left * this.sizeCell}%;`]);
+
+        // Set font size cells depend with cell size
+        this.cell.firstChild.style.fontSize = `${8 / this.size}rem`;
+
+        this.cell.style.width = `${this.sizeCell}%`;
+        this.cell.style.height = `${this.sizeCell}%`;
+        console.log(this.cell.firstChild);
+        if (this.numbersOn === true) {
+          this.menu.lastChild.innerText = 'Hide Numbers';
+          this.cell.firstChild.classList.add('cell-inner--show-numbers');
+        }
+
+        this.cells.push({
           number: elem.number,
           left: elem.left,
           top: elem.top,
-        };
+          item: this.cell,
+          active: false,
+        });
 
-        this.cells.push(this.emptyCell);
+        this.cell.addEventListener('click', () => this.cellMove(i));
 
-        return;
-      }
-      this.cell = create('div', 'cell-outer',
-        create('div', 'cell-inner', `${elem.number}`, null), this.field,
-        ['id', `${elem.number}`],
-        ['style', `top: ${elem.top * this.sizeCell}%; left: ${elem.left * this.sizeCell}%;`]);
-
-      // this.cell = create('div', 'cell', `${elem.number}`, this.field, ['id', `${elem.number}`],
-      //   ['style', `top: ${elem.top * this.sizeCell}%; left: ${elem.left * this.sizeCell}%;`]);
-
-      this.cell.style.width = `${this.sizeCell}%`;
-      this.cell.style.height = `${this.sizeCell}%`;
-
-      this.cells.push({
-        number: elem.number,
-        left: elem.left,
-        top: elem.top,
-        item: this.cell,
-        active: false,
+        this.cell.firstChild.style.backgroundImage = this.backgroundImage;
+        this.cell.firstChild.style.backgroundRepiat = 'no-repeat';
+        this.cell.firstChild.style.backgroundSize = `${100 * this.size}%`;
+        const numberCell = this.cells[this.cells.length - 1].number - 1;
+        const multiplier = 100 / (this.size - 1);
+        this.cell.firstChild.style.backgroundPosition = `${(numberCell % this.size) * multiplier}% ${Math.floor(numberCell / this.size) * multiplier}%`;
       });
+    }, 500);
 
-      this.cell.addEventListener('click', () => this.cellMove(i));
-    });
-    console.log('this.cells end', this.cells);
+    this.btnPress('GAME LOADED');
+  }
+
+  changeFieldSize() {
+    this.menuFieldSize.classList.toggle('menu-field-size--active');
   }
 
   switchSound = (e) => {
@@ -322,16 +365,38 @@ export default class Game {
     if (this.soundOn) {
       soundButton.innerHTML = '<span class="material-icons">volume_off</span>';
       this.soundOn = false;
+      this.btnPress('SOUND OFF');
     } else {
       soundButton.innerHTML = '<span class="material-icons">volume_up</span>';
       this.soundOn = true;
+      this.btnPress('SOUND ON');
+    }
+  }
+
+  showNumbers = (e) => {
+    console.log(e);
+    if (!this.numbersOn) {
+      this.numbersOn = true;
+      e.target.innerText = 'Hide Numbers';
+      this.btnPress('NUMBERS ADDED');
+      this.cells.forEach((elem) => {
+        if (!elem.item) return;
+        elem.item.firstChild.classList.add('cell-inner--show-numbers');
+      });
+    } else {
+      this.numbersOn = false;
+      e.target.innerText = 'Show Numbers';
+      this.btnPress('NUMBERS REMOVED');
+      this.cells.forEach((elem) => {
+        if (!elem.item) return;
+        elem.item.firstChild.classList.remove('cell-inner--show-numbers');
+      });
     }
   }
 
   updateCountMove() {
     this.countMoves += 1;
-    counts.firstChild.innerHTML = `Moves: ${this.countMoves}`;
-    console.log(this.countMoves);
+    this.counts.firstChild.innerHTML = `Moves: ${this.countMoves}`;
   }
 
   updateCountTimes = () => {
@@ -339,8 +404,17 @@ export default class Game {
     this.seconds = this.countTime % 60;
     this.minutes = Math.floor(this.countTime / 60);
 
-    counts.childNodes[1].innerHTML = `Time: ${this.minutes}m : ${this.seconds}s`;
+    this.counts.childNodes[1].innerHTML = `Time: ${this.minutes}m : ${this.seconds}s`;
 
     this.timeOut = setTimeout(this.updateCountTimes, 1000);
+  }
+
+  btnPress(str) {
+    this.buttonPress.classList.toggle('button-press--active');
+    this.buttonPress.innerText = `${str}`;
+    setTimeout(() => {
+      this.buttonPress.classList.toggle('button-press--active');
+      this.buttonPress.innerText = '';
+    }, 1500);
   }
 }
