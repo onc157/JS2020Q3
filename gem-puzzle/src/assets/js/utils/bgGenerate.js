d8a6ba5591b6448533ecda5645b5009b71842def
onc157 <onc157@yandex.ru>
2020-11-17 00:55:44 +0300

    fix: bg url

--- gem-puzzle/src/assets/js/utils/bgGenerate.js
+++ gem-puzzle/src/assets/js/utils/bgGenerate.js
@@ -2,5 +2,5 @@ export default function bgGenerate(a, b) {
   const rand = a + Math.random() * (b - a);
   let result = Math.round(rand);
   if (result < 10) result = `0${result}`;
-  return `url('../../../assets/img/${result}.jpg')`;
+  return `url('./assets/img/${result}.jpg')`;
 }
