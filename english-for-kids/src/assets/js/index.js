1dbfd8b171d1e3b97f5110f6bd383a4e3241d734
onc157 <onc157@yandex.ru>
2020-11-27 19:06:41 +0300

    init: start task english for kids

---
+++ english-for-kids/src/assets/js/index.js
@@ -0,0 +1,6 @@
+import '../css/style.scss';
+import './utils/create';
+import './utils/storage';
+// import Game from './main';
+
+// new Game(4).init().generateField();
---
+++ english-for-kids/src/assets/js/main.js
@@ -0,0 +1,10 @@
+// import create from './utils/create';
+// import * as storage from './utils/storage';
+// import audioClick from '../audio/click-cell.wav';
+// import audioButton from '../audio/click-button.wav';
+// import bgGenerate from './utils/bgGenerate';
+
+export default class Game {
+  constructor() {
+  }
+}
